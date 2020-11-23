ee827c2cfd20b45af27a589dfe4d241b81f32abd
soabel <alfredo.benaute@gmail.com>
2020-11-23 18:01:15 -0500

    proyecto de inicio

---
+++ src/test/java/com/bootcamp/adventureworks/categories/CategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.adventureworks.categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
